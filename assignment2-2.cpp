b547737e82719016767f654973e121939667feea
haileyavila16 <77656592+haileyavila16@users.noreply.github.com>
2021-10-04 11:36:09 -0700

    assignment 2-2

---
+++ assignment2-2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void printArray(double * const n, int N);
+
+int main()
+{
+    const int N = 10;
+    double numbers[N] = {10.0, 15.2, 21.2, 10.5, 11.3, 12.5, 13.8, 14.7, 19.8, 20.0};
+
+    printArray(numbers, N);
+    return 0;
+}
+void printArray(double * const n, int N);
+{
+    for(int i=0;i<N;i++){
+        cout << n[i] << " ";
+    }
+    cout << endl;
+}
