0f890f9d67323b5252d610790120d5c49e06d225
haileyavila16 <77656592+haileyavila16@users.noreply.github.com>
2021-10-11 17:48:51 -0700

    assignment  2-3

---
+++ assignment2-3.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+double *makeNumber()
+{
+    double *arr = new double[10];
+    for(int i=0;i<10;i++)
+    {
+        arr[i] = (double)rand() / RAND_MAX;
+    }
+    return arr;
+}
+void sortNumbers(double *arr)
+{
+    int n = 10;
+    double temp;
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=i+1;j<n-i-1;j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
+void printNumbers(double *arr)
+{
+    int n = 10;
+    for(int i=0;i<n;i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}   
+void deleteNumbers(double *arr)
+{
+    delete arr;
+}
+int main()
+{
+    double *numbers;
+    numbers = makeNumbers();
+    cout << "The numbers before sorting are: ";
+    printNumbers(numbers);
+    sortNumbers(numbers);
+    cout << "The numbers after sorting are: ";
+    printNumbers(numbers);
+    deleteNumbers(numbers);
+    return ;
+}
