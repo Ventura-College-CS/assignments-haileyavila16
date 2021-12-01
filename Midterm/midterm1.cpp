d909d88896582c84b45b45987bb64b497f04242f
haileyavila16 <77656592+haileyavila16@users.noreply.github.com>
2021-11-30 23:48:58 -0800

    q5

--- Midterm/midterm1.cpp
+++ Midterm/midterm1.cpp
@@ -51,3 +51,27 @@ class Stack
         }
         int size() {return pool.size();}
 };
+int main() 
+{
+    Student s1(1, "Jess", new double[3]{1, 2, 3});
+    Student s2(1, "Jay", new double[3]{4, 5, 6});
+    Student s3(1, "Alex", new double[3]{7, 8, 9});
+    Student s4(1, "Lexi", new double[3]{10, 11, 12});
+    Student s5(1, "Ray", new double[3]{13, 14, 15});
+
+    Stack<Student> st;
+    st.push(s1);
+    st.push(s2);
+    st.push(s3);
+    st.push(s4);
+    st.push(s5);
+
+    cout << "Size: " << st.size() << endl;
+    while(!st.isEmpty())
+    {
+        Student res = st.pop();
+        cout << "SID: " << res.getID() << " ,Sname: ";
+        cout << res.getScores()[0] << " , " << endl;    
+    }
+    return 0;
+}    
