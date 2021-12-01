cbdde98242f05c8500ae5ccd21484dc879039ad8
haileyavila16 <77656592+haileyavila16@users.noreply.github.com>
2021-11-30 23:05:22 -0800

    Q5

--- Midterm/midterm1.cpp
+++ Midterm/midterm1.cpp
@@ -4,5 +4,15 @@ using namespace std;
 
 class Student
 {
-    
+   private: 
+       int SID;
+       string Sname;
+       double *scores;
+    public:
+        Student()
+        {
+            SID=0;
+            Sname=" ";
+            scores=nullptr;
+        }
 }
