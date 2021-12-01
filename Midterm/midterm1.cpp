45e4736270ce63fb0c8f7a68280246dfe80b3a43
haileyavila16 <77656592+haileyavila16@users.noreply.github.com>
2021-11-30 23:17:52 -0800

    Q5

--- Midterm/midterm1.cpp
+++ Midterm/midterm1.cpp
@@ -11,8 +11,17 @@ class Student
     public:
         Student()
         {
-            SID=0;
-            Sname=" ";
-            scores=nullptr;
+            SID = 0;
+            Sname = " ";
+            scores = nullptr;
         }
-}
+        Student(int i, string name, double *sc)
+        {
+            SID = i;
+            Sname = name;
+            scores = sc;
+        }
+        int getID () {return SID;}
+        string getName() {return Sname;}
+        double *getScores() {return scores;}
+};
