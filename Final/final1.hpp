12148a10876bcb6f41043be6af1ba11105be0416
haileyavila16 <77656592+haileyavila16@users.noreply.github.com>
2021-12-13 16:48:22 -0800

    Q1

--- Final/final1.hpp
+++ Final/final1.hpp
@@ -10,5 +10,13 @@ class Course
         string name;
         int credit;
     public:
-    
+        Course();
+        Course(int id, string name, string credit);
+        int getId() const;
+        const string &getName() const;
+        int getCredit() const;
+        void setId(int id);
+        void setName(const string &name);
+        void setCredit(int credit);
+        void print() const;
 }
