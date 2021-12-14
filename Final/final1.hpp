511a90d95807078ebaa2c0a33419f386d99c822d
haileyavila16 <77656592+haileyavila16@users.noreply.github.com>
2021-12-13 16:44:05 -0800

    Q1

---
+++ Final/final1.hpp
@@ -0,0 +1,14 @@
+#ifndef FINAL1_H
+#define FINAL1_H
+#include <iostream>
+using namespace std;
+
+class Course
+{
+    private:
+        int id;
+        string name;
+        int credit;
+    public:
+    
+}
