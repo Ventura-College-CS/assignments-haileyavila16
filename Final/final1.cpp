5313a44bc871256b3bb781c3b46959fa6cb98139
haileyavila16 <77656592+haileyavila16@users.noreply.github.com>
2021-12-13 18:43:43 -0800

    Q3

--- Final/final1.cpp
+++ Final/final1.cpp
@@ -1,12 +1,29 @@
 #include "final1.hpp"
 
-Course::Course()
-{
-    id=0;
-    name="";
-    credit=0;
+Course::Course() {
+    id = 0;
+    name = "";
+    credit = 0;
 }
 Course::Course(int id, string name, int credit) : id(id), name(name), credit(credit) {}
-
-int C
-
+int Course::getId() const {
+    return id;
+}
+const string &Course::getName() const {
+    return name;
+}
+int Course::getCredit() const {
+    return credit;
+}
+void Course::setId(int id) {
+    this->id = id;
+}
+void Course::setName(const string &name) {
+    this->name = name;
+}
+void Course::setCredit(int credit) {
+    this->credit = credit;
+}
+void Course::print() const {
+    cout << "Course{id=" << id << ", name=" << name << ", credit=" << credit << "}" << endl;
+}
