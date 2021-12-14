354d02b2cfd707daa84e2e2beee86feb1cbef764
haileyavila16 <77656592+haileyavila16@users.noreply.github.com>
2021-12-13 17:04:46 -0800

    Q1

---
+++ Final/final1.cpp
@@ -0,0 +1,12 @@
+#include "final1.hpp"
+
+Course::Course()
+{
+    id=0;
+    name="";
+    credit=0;
+}
+Course::Course(int id, string name, int credit) : id(id), name(name), credit(credit) {}
+
+int C
+
